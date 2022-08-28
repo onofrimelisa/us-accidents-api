b96da09834b948e30ae4b45a02c271ec4137d3b9
Melisa Ailen Onofri <melisa.onofri@kavak.com>
2022-08-28 19:17:27 -0300

    feat: add swagger ui

--- accidents/src/main/java/com/unlp/bbdd2/accidents/controller/AccidentController.java
+++ accidents/src/main/java/com/unlp/bbdd2/accidents/controller/AccidentController.java
@@ -4,6 +4,7 @@ import com.unlp.bbdd2.accidents.dto.CircleRequestDTO;
 import com.unlp.bbdd2.accidents.dto.PolygonRequestDTO;
 import com.unlp.bbdd2.accidents.model.Accident;
 import com.unlp.bbdd2.accidents.service.AccidentService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.geo.Circle;
@@ -23,21 +24,25 @@ public class AccidentController {
     @Autowired
     private AccidentService accidentService;
 
+    @Operation(summary = "Get an accident by its id", tags = "Accidents")
     @GetMapping("/accident/{id}")
     public Accident findById(@PathVariable("id") String id) {
         return this.accidentService.findById(id);
     }
 
+    @Operation(summary = "Get all the accidents", tags = "Accidents")
     @GetMapping("/accident")
     public Page<Accident> findAll() {
         return this.accidentService.findAll();
     }
 
+    @Operation(summary = "Get all the accidents within a given polygon", tags = "Accidents")
     @PostMapping("/accident/polygon")
     public Page<Accident> findFromPolygon(@Valid @RequestBody PolygonRequestDTO polygonRequestDTO) {
         return this.accidentService.findByStartPointWithin(this.mapPolygonRequestDTOToPolygon(polygonRequestDTO));
     }
 
+    @Operation(summary = "Get all the accidents within a given circle", tags = "Accidents")
     @PostMapping("/accident/circle")
     public Page<Accident> findFromCircle(@Valid @RequestBody CircleRequestDTO circleRequestDTO) {
         return this.accidentService.findByStartPointWithin(this.mapCircleRequestDTOToCircle(circleRequestDTO));
